94c24888ac971f0bf698988d196b8c0654c2198c
Ángela Martín Pérez <903916@unizar.es>
2026-03-19 18:34:12 +0100

    Changes

--- djangotutorial/build.sh
+++ djangotutorial/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 pip install -r requirements.txt
-py manage.py collectstatic --noinput
-py manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
--- djangotutorial/start.sh
+++ djangotutorial/start.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 gunicorn mysite.wsgi:application 
